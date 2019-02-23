35fbf18ae8cae14551915a7754edc1e25f2cb485
Zhuguang Jiang <Jason@zhuguangs-mbp-2.wireless.rit.edu>
2019-02-22 20:19:37 -0500

    Add Button.

--- MyHero/ViewController.swift
+++ MyHero/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var heath_Value : Int = 100
+    private var attack_Value : Int = 10
+    private var strength_Value : Double = 1.0
+    private var ailge_Value : Double = 1.0
+    
+    private var heath_Label = UILabel.init()
+    private var attack_Label = UILabel.init()
+    private var strength_Label = UILabel.init()
+    private var ailge_Label = UILabel.init()
+    private var crit_Label = UILabel.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setting()
@@ -23,6 +34,8 @@ class ViewController: UIViewController {
         let halfHeight = fullHeight/2
         let oneOfThridWidth = fullWidth/3
         let oneOfThridHeight = fullHeight/3
+        let oneOfFourth = fullWidth/4
+        let oneOfFifty = fullWidth/5
         
         
         let background = UIImageView.init(frame: CGRect(x: 0, y: 0, width: fullWidth, height: fullHeight))
@@ -39,28 +52,68 @@ class ViewController: UIViewController {
         level_Label.text = "Level : 1"
         view.addSubview(level_Label)
         
-        let heath_Label = UILabel.init(frame: CGRect(x: 10, y: hero.frame.origin.y, width: fullWidth/4, height: 50))
-        heath_Label.text = "Heath : 100"
+        heath_Label.frame = CGRect(x: 10, y: hero.frame.origin.y, width: fullWidth/4, height: 50)
+        heath_Label.text = "Heath : \(heath_Value)"
         view.addSubview(heath_Label)
         
-        let Attack_Label = UILabel.init(frame: CGRect(x: 10, y: hero.frame.origin.y+fullHeight/9, width: fullWidth/4, height: 50))
-        Attack_Label.text = "Attack : 10"
-        view.addSubview(Attack_Label)
+        attack_Label.frame = CGRect(x: 10, y: hero.frame.origin.y+fullHeight/9, width: fullWidth/4, height: 50)
+        attack_Label.text = "Attack : \(attack_Value)"
+        view.addSubview(attack_Label)
         
-        let strength_Label = UILabel.init(frame: CGRect(x: hero.frame.origin.x+fullWidth/3+10, y: hero.frame.origin.y, width: fullWidth/4, height: 50))
-        strength_Label.text = "Strength : 1.0"
+        strength_Label.frame = CGRect(x: hero.frame.origin.x+fullWidth/3+10, y: hero.frame.origin.y, width: fullWidth/4, height: 50)
+        strength_Label.text = "Strength : \(strength_Value)"
         view.addSubview(strength_Label)
         
-        let alige_Label = UILabel.init(frame: CGRect(x: hero.frame.origin.x+fullWidth/3+10, y: hero.frame.origin.y+fullHeight/9, width: fullWidth/4, height: 50))
-        alige_Label.text = "Alige : 1.0"
-        view.addSubview(alige_Label)
+        ailge_Label.frame = CGRect(x: hero.frame.origin.x+fullWidth/3+10, y: hero.frame.origin.y+fullHeight/9, width: fullWidth/4, height: 50)
+        ailge_Label.text = "Alige : \(ailge_Value)"
+        view.addSubview(ailge_Label)
         
-        let crit_Label = UILabel.init(frame: CGRect(x: hero.frame.origin.x+fullWidth/3+10, y: hero.frame.origin.y+fullHeight/9*2, width: fullWidth/4, height: 50))
+        crit_Label.frame =  CGRect(x: hero.frame.origin.x+fullWidth/3+10, y: hero.frame.origin.y+fullHeight/9*2, width: fullWidth/4, height: 50)
         crit_Label.text = "Crit : 0.0"
         view.addSubview(crit_Label)
         
+        let heath_Button = UIButton(frame: CGRect(x: 0, y: crit_Label.frame.origin.y+50, width: oneOfFifty, height: 50))
+        heath_Button.setTitle("Heath", for: .normal)
+        heath_Button.setTitleColor(UIColor.black, for: .normal)
+        heath_Button.backgroundColor = UIColor.lightGray
+        heath_Button.addTarget(self, action: #selector(addValue), for: .touchUpInside)
+        heath_Button.tag = 1
+        view.addSubview(heath_Button)
+        
+        let attack_Button = UIButton(frame: CGRect(x: oneOfFourth, y: crit_Label.frame.origin.y+50, width: oneOfFifty, height: 50))
+        attack_Button.setTitle("Attack", for: .normal)
+        attack_Button.setTitleColor(UIColor.black, for: .normal)
+        attack_Button.backgroundColor = UIColor.lightGray
+        attack_Button.addTarget(self, action: #selector(addValue), for: .touchUpInside)
+        attack_Button.tag = 2
+        view.addSubview(attack_Button)
+        
+        let strength_Button = UIButton(frame: CGRect(x: 2*oneOfFourth, y: crit_Label.frame.origin.y+50, width: oneOfFifty, height: 50))
+        strength_Button.setTitle("Strength", for: .normal)
+        strength_Button.setTitleColor(UIColor.black, for: .normal)
+        strength_Button.backgroundColor = UIColor.lightGray
+        strength_Button.addTarget(self, action: #selector(addValue), for: .touchUpInside)
+        strength_Button.tag = 3
+        view.addSubview(strength_Button)
+        
+        let Ailge_Button = UIButton(frame: CGRect(x: 3*oneOfFourth, y: crit_Label.frame.origin.y+50, width: oneOfFifty, height: 50))
+        Ailge_Button.setTitle("Alige", for: .normal)
+        Ailge_Button.setTitleColor(UIColor.black, for: .normal)
+        Ailge_Button.backgroundColor = UIColor.lightGray
+        Ailge_Button.addTarget(self, action: #selector(addValue), for: .touchUpInside)
+        Ailge_Button.tag = 4
+        view.addSubview(Ailge_Button)
+    }
+    @objc func addValue(_ sender: UIButton){
+        switch (sender.tag) {
+        case 4 :  ailge_Value += 0.1; ailge_Label.text = "Ailge: "+String(format: "%.1f", ailge_Value)
+        case 1 :  heath_Value += 10; heath_Label.text = "Heath : \(heath_Value)"
+        case 3 :  strength_Value += 0.1; strength_Label.text = "Strength: "+String(format: "%.1f", strength_Value)
+        case 2 :  attack_Value += 1; attack_Label.text = "Attack : \(attack_Value)"
+        default:
+            fatalError()
+        }
         
     }
-
 }
 
